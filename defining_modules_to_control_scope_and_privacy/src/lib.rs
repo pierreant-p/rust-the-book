944717a78eefa4a1ec925999e7ca78b348787783
Pierre-Antoine Passet <pa@passet.org>
2020-07-12 09:31:14 +0200

    [Packages, Crates And Modules] 7.2 - Defining Modules to Control Scope and Privacy

---
+++ defining_modules_to_control_scope_and_privacy/src/lib.rs
@@ -0,0 +1,17 @@
+// parent module
+mod front_of_house {
+    // nested module
+    mod hosting {
+        fn add_to_waitlist() {}
+
+        fn seat_at_table() {}
+    }
+
+    mod serving {
+        fn take_order() {}
+
+        fn serve_order() {}
+
+        fn take_payment() {}
+    }
+}
