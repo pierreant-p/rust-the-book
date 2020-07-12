251bcf086bcee07ac845ad9581a73380d2e1e107
Pierre-Antoine Passet <pa@passet.org>
2020-07-12 10:20:46 +0200

    [Packages, Crates And Modules] 7.5 - Separating Modules into Different Files

---
+++ separating_modules_into_different_files/src/lib.rs
@@ -0,0 +1,11 @@
+// declare module
+// rust will try to find the file with the code
+mod front_of_house;
+
+pub use crate::front_of_house::hosting;
+
+pub fn eat_at_restaurant() {
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+}
