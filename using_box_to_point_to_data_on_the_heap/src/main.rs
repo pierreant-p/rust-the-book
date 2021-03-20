d30dc821d236ec076f7312b2f13f3af8e6cd53b8
Pierre-Antoine Passet <pa@passet.org>
2021-03-20 18:11:20 +0100

    15.1 - Using Box<T> to Point to Data on the Heap

---
+++ using_box_to_point_to_data_on_the_heap/src/main.rs
@@ -0,0 +1,17 @@
+// This would cause an error because the size can't be known
+// at compile time.
+//
+// enum List {
+//     Cons(i32, List),
+//     Nil,
+// }
+
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
+}
+
+fn main() {
+    let b = Box::new(5);
+    println!("b = {}", b);
+}
